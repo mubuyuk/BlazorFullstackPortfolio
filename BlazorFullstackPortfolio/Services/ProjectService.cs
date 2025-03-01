f9aa4e69fd9acc557aacf81a4ffb321d6c368ddd
mubuyuk <muratbuyuksal@gmail.com>
2025-03-01 12:23:29 +0100

    Add project files.

---
+++ BlazorFullstackPortfolio/Services/ProjectService.cs
@@ -0,0 +1,40 @@
+﻿using BlazorFullstackPortfolio.Models;
+
+namespace BlazorFullstackPortfolio.Services
+{
+    public class ProjectService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProjectService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // Hämta alla Projects
+        public async Task<List<Project>> GetProjectsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Project>>("api/projects") ?? new List<Project>();
+        }
+        // Lägg till ett nytt Project
+        public async Task<bool> AddProjectAsync(Project project)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/projects", project);
+            return response.IsSuccessStatusCode;
+        }
+
+        // Uppdatera ett Project
+        public async Task<bool> UpdateProjectAsync(Project project)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/projects/{project.Id}", project);
+            return response.IsSuccessStatusCode;
+        }
+
+        // Ta bort ett Project
+        public async Task<bool> DeleteProjectAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/projects/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ BlazorFullstackPortfolio/wwwroot/bootstrapModal.js
@@ -0,0 +1,14 @@
+﻿window.bootstrapModal = {
+    show: (id) => {
+        var modal = new bootstrap.Modal(document.querySelector(id));
+        modal.show();
+    },
+    hide: (id) => {
+        var modalElement = document.querySelector(id);
+        var modal = bootstrap.Modal.getInstance(modalElement);
+        if (modal) {
+            modal.hide();
+        }
+    }
+};
+
